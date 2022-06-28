4e9ffe112b1ddf57d0594244f1806b7b93453e44
Loveneesh <deepakmehra149@gmail.com>
2022-06-28 16:10:00 +0530

    created test cases for reservation movements aff and tandem reservations

--- test/specs/Reservation_Movement_Testing/C_ChangeReservation__Aff_Link.js
+++ test/specs/Reservation_Movement_Testing/C_ChangeReservation__Aff_Link.js
@@ -7,14 +7,14 @@ import Manifest_Reservation_Testing_Utils from '../../Utils/Manifest_Reservation
 
 describe("create aff reservation, change reservation from email link and verify the timeslots",()=>{
     it("create aff reservation, change reservation from email link and verify the timeslots" , async()=>{
+        
+        //create reservation
         const numOfTickets = 1;
         await ReservationPage.vist_reservation_page();
         await browser.pause(500);
         // Date index is the index of date in calender index starts from 1 
         // The very first date in the calender is at index 1 then index increases by 1 
         const DateIndex = 25;
-    
-
         var {selected_date,selected_month,seats} = await Reservation_Testing_Utils.select_tandemP_date(7,DateIndex,"","aff");
         await Reservation_Testing_Utils.select_num_tickets_pay_option(numOfTickets,"full");
         const jumperNames = await Reservation_Testing_Utils.fill_passenger_details_aff_res(numOfTickets,true);
@@ -25,12 +25,10 @@ describe("create aff reservation, change reservation from email link and verify 
         await browser.pause(1000);
         const reservationId = await Reservation_Movement_Utils.get_reservation_id(jumperNames);
 
-        // // //visit change reservation link
+        //visit change reservation link
         await browser.pause(2000);
         await ChangeReservation_Page.visit_change_reservation_page(reservationId);
         await browser.pause(1000);
-
-
         const dateSel = await ChangeReservation_Page.get_date_sel(26);
         await dateSel.click();
         await browser.pause(1000);
@@ -39,12 +37,16 @@ describe("create aff reservation, change reservation from email link and verify 
         numOfSeatsPrev = Number((numOfSeatsPrev.split(" "))[3])
         await ChangeReservation_Page.seats_sel.click();
         await ChangeReservation_Page.change_date_btn.click();
+
+        //verify timeslots seats reservation side
         await ChangeReservation_Page.visit_change_reservation_page(reservationId);
         await browser.pause(1000);
         await dateSel.click();
         let numOfSeatsAft = await ChangeReservation_Page.seats_sel.getText();
         numOfSeatsAft = Number((numOfSeatsAft.split(" "))[3])
         await expect(numOfSeatsPrev-1).toEqual(numOfSeatsAft);
+
+        //verfiy movement manifest side
         await browser.pause(1000);
         await Manifest_Reservation_Testing_Utils.verify_reservation_manifest_side(selected_new_date,selected_month,jumperNames[0],invoiceTotalVal,false,true);
         await browser.pause(7000);
